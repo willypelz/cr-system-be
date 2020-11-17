a692be5a7520ad29fe0b0287564e5287c875d0be
willypelz <pelumiasefon@gmail.com>
2020-11-17 05:17:27 +0100

    [IMP] add user seeder

---
+++ src/seeder/user.seeder.ts
@@ -0,0 +1,25 @@
+import { Connection, Repository } from "typeorm"
+import * as yaml from 'js-yaml'
+import * as fs from 'fs'
+
+export async function loadFixtures(name: string, dbConnection: Connection): Promise<any> {
+
+    let items: any[] = []
+    try {
+        const file: any = yaml.safeLoad(fs.readFileSync(`./${name}.yml`, 'utf8'));
+        console.log(file)
+        items = file["fixtures"]
+    } catch (e) {
+        console.log("fixtures error", e);
+    }
+
+    if(!items) {
+        return
+    }
+
+    items.forEach(async (item: any) => {
+        const entityName = Object.keys(item)[0]
+        const data = item[entityName]
+        await dbConnection.createQueryBuilder().insert().into(entityName).values(data).execute()
+    })
+}
